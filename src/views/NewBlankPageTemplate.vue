b616bd001c484b05f6a03bffc77a4ec1413baabc
Pirijan <hi@pirijan.com>
2026-05-05 13:20:40 -0400

    noop add blank page template filte

---
+++ src/views/NewBlankPageTemplate.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { reactive, computed, onMounted, onUnmounted, onBeforeUnmount, watch, ref, nextTick } from 'vue'
+
+import { useGlobalStore } from '@/stores/useGlobalStore'
+import { useThemeStore } from '@/stores/useThemeStore'
+import { useUserStore } from '@/stores/useUserStore'
+
+import AboutJsonLd from '@/components/page/about/AboutJsonLd.vue'
+import Header from '@/components/page/Header.vue'
+import Wordmark from '@/components/page/Wordmark.vue'
+import FooterSitemap from '@/components/page/FooterSitemap.vue'
+import Footer from '@/components/page/Footer.vue'
+import consts from '@/consts.js'
+
+const globalStore = useGlobalStore()
+const themeStore = useThemeStore()
+const userStore = useUserStore()
+
+const appsButtonElement = ref(null)
+let unsubscribes
+
+if (!consts.isStaticPrerenderingPage) {
+  window.globalStore = useGlobalStore()
+  window.themeStore = useThemeStore()
+  if (consts.isDevelopment()) {
+    window.userStore = useUserStore()
+  }
+}
+
+onMounted(() => {
+  if (!consts.isStaticPrerenderingPage) {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', logSystemThemeChange)
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateSystemTheme)
+    themeStore.restoreTheme()
+  }
+  if (consts.isDevelopment()) {
+    document.title = '[DEV] Kinopio Page Title'
+  } else {
+    document.title = 'Kinopio Page Title'
+  }
+})
+
+const closeAllDialogs = () => {
+  globalStore.closeAllDialogs()
+}
+
+// theme
+
+const isThemeDark = computed(() => themeStore.getIsThemeDark)
+const logSystemThemeChange = (event) => {
+  const themeIsSystem = userStore.themeIsSystem
+  console.warn('🌓 logSystemThemeChange', window.matchMedia('(prefers-color-scheme: dark)'), event, { themeIsSystem })
+}
+const updateSystemTheme = () => {
+  themeStore.updateSystemTheme()
+}
+</script>
+
+<template lang="pug">
+AboutJsonLd
+.page(:class="{ 'is-dark-theme': isThemeDark }")
+  Header(:isDocumentPage="true")
+  main.page(@click="closeAllDialogs")
+    .page-wrap
+      section.intro
+        Wordmark
+        h2 Page Title
+
+        p [page contents]
+
+      FooterSitemap
+  Footer
+</template>
+
+<style lang="stylus">
+</style>
